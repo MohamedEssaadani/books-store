573b363160c3f24e5fc9df77c6f9e2396979ddc0
Mohamed Es-saadani <essaadani80@gmail.com>
2021-12-18 21:26:15 +0100

    'fc'

---
+++ BooksStore/ClientApp/src/App.js
@@ -0,0 +1,63 @@
+import {Sidebar} from "./components/Sidebar";
+import {BrowserRouter as Router, Switch, Route, Link, useHistory} from "react-router-dom";
+import {Dropdown} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPlus, faSignOutAlt, faUser, faUsers} from "@fortawesome/free-solid-svg-icons";
+
+
+function App() {
+
+
+    return (
+        <div className="App">
+            <Router>
+                <div id="wrapper">
+                    <Sidebar/>
+                    <div id="content-wrapper" className="d-flex flex-column">
+                        <div id="content">
+                            <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                                <button
+                                    id="sidebarToggleTop"
+                                    className="btn btn-link d-md-none rounded-circle mr-3"
+                                >
+                                    <i className="fa fa-bars"></i>
+                                </button>
+
+                                <ul className="navbar-nav ml-auto">
+                                    <li className="d-inline-block ml-auto ml-md-3 my-2 my-md-0 mw-100 pt-2">
+                                        <Dropdown className="nav-item">
+                                            <Dropdown.Toggle className="btn btn-default"
+                                                             style={{backgroundColor: "white", color: "black"}}>
+                                                <FontAwesomeIcon icon={faUser}/>
+                                                {" ES-SAADANI Mohamed"}
+                                            </Dropdown.Toggle>
+
+                                            <Dropdown.Menu>
+                                                <Dropdown.Item
+                                                >
+                                                    <FontAwesomeIcon icon={faUser}/>
+                                                    {" "}<span> Profil</span>
+                                                </Dropdown.Item>
+                                                <Dropdown.Item >
+                                                    <FontAwesomeIcon icon={faSignOutAlt}/>
+                                                    {" "}<span> Déconnecter</span>
+                                                </Dropdown.Item>
+                                            </Dropdown.Menu>
+                                        </Dropdown>
+
+                                    </li>
+                                </ul>
+                            </nav>
+                            <Switch>
+                                <Route path="/" exact/>
+                            </Switch>
+                        </div>
+                    </div>
+                </div>
+            </Router>
+        </div>
+    )
+        ;
+}
+
+export default App;
---
+++ BooksStore/ClientApp/src/components/Sidebar.js
@@ -0,0 +1,115 @@
+import * as React from 'react';
+import {Link} from 'react-router-dom'
+import {Dropdown} from "react-bootstrap";
+import {LinkContainer} from "react-router-bootstrap";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {
+    faBook,
+    faChartArea,
+    faList,
+    faPlus, faTabletAlt,
+    faUser,
+    faUsers
+} from '@fortawesome/free-solid-svg-icons'
+
+export function Sidebar() {
+    return (
+        <>
+            <ul
+                className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
+                id="accordionSidebar"
+            >
+                <a className="sidebar-brand d-flex align-items-center justify-content-center">
+                    <div className="sidebar-brand-icon rotate-n-15">
+                        <FontAwesomeIcon icon={faBook}/>
+                    </div>
+                    <div className="sidebar-brand-text mx-3">Books Store</div>
+                </a>
+
+                <hr className="sidebar-divider my-0"/>
+
+                <li className="nav-item active">
+                    <Link className="nav-link" to="/admin/">
+                        <FontAwesomeIcon icon={faChartArea}/>
+                        {" "}<span>Tableau de board</span>
+                    </Link>
+                </li>
+
+                <hr className="sidebar-divider"/>
+
+                <div className="sidebar-heading">Books Management</div>
+
+                <Dropdown className="nav-item">
+                    <Dropdown.Toggle className="nav-link myDrop">
+                        <FontAwesomeIcon icon={faBook} />
+                        {" "}Books
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <LinkContainer to={"/admin/customers"}>
+                            <Dropdown.Item>
+                                <FontAwesomeIcon icon={faBook} />
+                                {" "}<span>Books</span>
+                            </Dropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to={"/admin/new-customer"}>
+                            <Dropdown.Item>
+                                <FontAwesomeIcon icon={faPlus}/>
+                                {" "}<span>New Book</span>
+                            </Dropdown.Item>
+                        </LinkContainer>
+                    </Dropdown.Menu>
+                </Dropdown>
+
+                <hr className="sidebar-divider"/>
+
+                <div className="sidebar-heading">Categories Management</div>
+                <Dropdown className="nav-item">
+                    <Dropdown.Toggle className="nav-link myDrop">
+                        <FontAwesomeIcon icon={faList}/>
+                        {" "}Categories
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <LinkContainer to={"/admin/accounts"}>
+                            <Dropdown.Item>
+                                <FontAwesomeIcon icon={faList} />
+                                {" "}<span>Categories</span>
+                            </Dropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to={"/admin/new-account"}>
+                            <Dropdown.Item>
+                                <FontAwesomeIcon icon={faPlus}/>
+                                {" "}<span>New Category</span>
+                            </Dropdown.Item>
+                        </LinkContainer>
+                    </Dropdown.Menu>
+                </Dropdown>
+
+                <div className="sidebar-heading">Authors Management</div>
+                <Dropdown className="nav-item">
+                    <Dropdown.Toggle className="nav-link myDrop">
+                        <FontAwesomeIcon icon={faUsers} />
+                        {" "}Authors
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <LinkContainer to={"/admin/accounts"}>
+                            <Dropdown.Item>
+                                <FontAwesomeIcon icon={faUsers} />
+                                {" "}<span>Authors</span>
+                            </Dropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to={"/admin/new-account"}>
+                            <Dropdown.Item>
+                                <FontAwesomeIcon icon={faPlus} />
+                                {" "}<span>New Author</span>
+                            </Dropdown.Item>
+                        </LinkContainer>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </ul>
+        </>
+    )
+        ;
+}
