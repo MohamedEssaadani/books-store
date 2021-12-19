0105ec4ce2e1f4bd42890f7a1a36dfc4ef2af8e6
Mohamed Es-saadani <essaadani80@gmail.com>
2021-12-19 16:23:07 +0100

    'commit'

---
+++ BooksStore/Controllers/CategoriesController.cs
@@ -0,0 +1,81 @@
+﻿using BL.CategoryService;
+using BooksStore.DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BooksStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("/api/categories")]
+        public ActionResult GetCategories()
+        {
+            IEnumerable<Category> categories = _categoryService.GetAll();
+            if (categories == null || !categories.Any())
+                return NotFound("Not Categories Found!");
+
+            return Ok(categories);
+        }
+
+        [HttpGet("/api/categories/{id}")]
+        public ActionResult GetCategory(int id)
+        {
+            Category category = _categoryService.GetById(id);
+            if (category == null)
+                return NotFound($"No Category With ID : {id} Found!");
+            return Ok(category);
+        }
+
+        [HttpPost("/api/categories")]
+        public ActionResult CreateCategory([FromBody] Category category)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Model State is Not Valid!!");
+
+            _categoryService.Add(category);
+
+            return Ok($"Category : {category.CategoryName} Added Successfully!");
+        }
+
+        [HttpDelete("/api/categories/{id}")]
+        public ActionResult DeleteCategory(int id)
+        {
+            Category category = _categoryService.GetById(id);
+
+            if (category != null)
+            {
+                _categoryService.Delete(id);
+                return Ok($"Category with ID = {id} is Deleted Successfully.");
+            }
+            return NotFound($"No Category With ID : {id} Found!");
+        }
+
+        [HttpPut("/api/categories/{id}")]
+        public ActionResult UpdateCategory(int id, Category category)
+        {
+            if (id != category.CategoryId || !ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            if (_categoryService.GetById(id) == null)
+                return NotFound($"No Category With ID : {id} Found!");
+
+            var updated = _categoryService.GetById(id);
+            updated.CategoryName = category.CategoryName;
+            _categoryService.Update(updated);
+
+            return Ok($"Updated Successfully.");
+        }
+    }
+}
--- DAL/Seeders/SeedData.cs
+++ DAL/Seeders/SeedData.cs
@@ -21,29 +21,91 @@ namespace BooksStore.DAL.Seeders
                 context.Categories.AddRange(
                     new Category
                     {
-                        CategoryName = "Fantasy"
+                        CategoryName = "Autobiography"
                     },
-                     new Category
+                    new Category
                      {
-                         CategoryName = "Classics"
+                         CategoryName = "Romance"
                      },
 
-                     new Category
+                    new Category
                      {
-                         CategoryName = "Comic Book or Graphic Novel"
+                         CategoryName = "Teen & Young Adult"
                      },
 
-                     new Category
+                    new Category
                      {
                          CategoryName = "Literary Fiction"
                      },
 
-                      new Category
+                    new Category
                       {
                           CategoryName = "Horror"
                       }
 
                 );
+
+                context.SaveChanges();
+
+
+
+
+                context.Authors.AddRange(
+                     new Author
+                     {
+                          AuthorName = "Dr. A.P.J. Abdul Kalam"
+                     },
+                     new Author
+                     {
+                         AuthorName = "Lynsay Sands"
+                     },
+                     new Author
+                      {
+                          AuthorName = "Garth Nix"
+                     },
+                      new Author
+                       {
+                           AuthorName = "Insight Editions"
+                      }
+
+                );
+                context.SaveChanges();
+
+
+                context.Books.AddRange(
+                    new Book
+                    {
+                        BookName = "Wings of Fire",
+                        Author = context.Authors.SingleOrDefault(x => x.AuthorId == 1),
+                        AuthorId = context.Authors.SingleOrDefault(x => x.AuthorId == 1).AuthorId,
+                        category = context.Categories.SingleOrDefault(x => x.CategoryId == 1),
+                        CategoryId = context.Categories.SingleOrDefault(x => x.CategoryId == 1).CategoryId,
+                    },
+                     new Book
+                     {
+                         BookName = "Mile High with a Vampire [Large Print]",
+                         Author = context.Authors.SingleOrDefault(x => x.AuthorId == 2),
+                         AuthorId = context.Authors.SingleOrDefault(x => x.AuthorId == 2).AuthorId,
+                         category = context.Categories.SingleOrDefault(x => x.CategoryId == 2),
+                         CategoryId = context.Categories.SingleOrDefault(x => x.CategoryId == 2).CategoryId,
+                     },
+                      new Book
+                      {
+                          BookName = "Terciel & Elinor",
+                          Author = context.Authors.SingleOrDefault(x => x.AuthorId == 3),
+                          AuthorId = context.Authors.SingleOrDefault(x => x.AuthorId == 3).AuthorId,
+                          category = context.Categories.SingleOrDefault(x => x.CategoryId == 2),
+                          CategoryId = context.Categories.SingleOrDefault(x => x.CategoryId == 2).CategoryId,
+                      },
+                       new Book
+                       {
+                           BookName = "Harry Potter: Holiday Magic: The Official Advent Calendar",
+                           Author = context.Authors.SingleOrDefault(x => x.AuthorId == 4),
+                           AuthorId = context.Authors.SingleOrDefault(x => x.AuthorId == 4).AuthorId,
+                           category = context.Categories.SingleOrDefault(x => x.CategoryId == 3),
+                           CategoryId = context.Categories.SingleOrDefault(x => x.CategoryId == 3).CategoryId,
+                       }
+                    );
                 context.SaveChanges();
             }
         }
